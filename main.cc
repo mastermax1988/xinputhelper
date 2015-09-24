7f85e5953bd2d64d7143f4dd1d7cb027fe69b8d6
Maximilian Huber <huber.maxi@gmail.com>
2015-09-24 20:54:22 +0200

    it builds

--- main.cc
+++ main.cc
@@ -1,11 +1,6 @@
 #include <iostream>
 #include "include/command.hh"
 #include "include/myfile.hh"
-#include "include/myclass.hh"
-#include <boost/algorithm/string.hpp>
-#include <vector>
-#include <boost/lexical_cast.hpp>
-#include "include/board.hh"
 
 using namespace std;
 using namespace boost;
